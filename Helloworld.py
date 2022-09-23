36607c75112581c2a49477c613c64c4c624142a0
Endoplex <justin.nguyen.inv@gmail.com>
2022-09-22 20:43:58 -0500

    first "real" commit

--- Helloworld.py
+++ Helloworld.py
@@ -1,8 +1,10 @@
 import sys
 import cv2
 print("Hello World\n")
-print("I am python version: " + sys.version)
-
+print("I am python version: " + sys.version + 
+      "\nThis computer is using: openCV " + cv2.__version__ +
+      "\nPress the esc key to terminate the webcam feed")
+ 
 img = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 img.set(cv2.CAP_PROP_FRAME_HEIGHT, 400)
 img.set(cv2.CAP_PROP_FRAME_WIDTH, 600)
